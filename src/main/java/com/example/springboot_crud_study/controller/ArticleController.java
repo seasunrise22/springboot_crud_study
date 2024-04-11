8fbec02a31892a55bb7474b67cbd8b08caf90538
seasunrise22 <seasunrise22@gmail.com>
2024-04-11 10:49:36 +0900

    before install window

--- src/main/java/com/example/springboot_crud_study/controller/ArticleController.java
+++ src/main/java/com/example/springboot_crud_study/controller/ArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
 @Slf4j
 @Controller
 public class ArticleController {
@@ -37,7 +39,7 @@ public class ArticleController {
         return "";
     }
 
-    @GetMapping("articles/{id}") // 데이터 조회 요청 접수
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
     public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아 오기
         log.info("id = " + id); // id를 잘 받았는지 확인하는 로그 찍기
 
@@ -47,5 +49,16 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. DB에서 모든 Article 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 가져온 Article 묶음을 모델에 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 사용자에게 보여 줄 뷰 페이지 설정하기
+
+        return "articles/index";
     }
 }
