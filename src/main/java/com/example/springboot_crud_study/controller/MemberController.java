38f5b3513d4f5b294adc70741cff160270e85829
seasunrise22 <seasunrise22@gmail.com>
2024-04-13 00:01:18 +0900

    daily push

--- src/main/java/com/example/springboot_crud_study/controller/MemberController.java
+++ src/main/java/com/example/springboot_crud_study/controller/MemberController.java
@@ -3,11 +3,17 @@ package com.example.springboot_crud_study.controller;
 import com.example.springboot_crud_study.dto.MemberForm;
 import com.example.springboot_crud_study.entity.Member;
 import com.example.springboot_crud_study.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
@@ -31,5 +37,23 @@ public class MemberController {
         saved.toString();
 
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+
+        return "members/index";
     }
 }
--- src/main/java/com/example/springboot_crud_study/repository/MemberRepository.java
+++ src/main/java/com/example/springboot_crud_study/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.example.springboot_crud_study.repository;
 import com.example.springboot_crud_study.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    @Override
+    ArrayList<Member> findAll();
 }
