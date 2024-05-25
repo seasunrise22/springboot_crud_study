7b5add172643d8d199c8d7fe00e79018ff9af5f4
lee <seasunrise22@gmail.com>
2024-05-25 17:53:04 +0900

    Testing in progress

---
+++ src/test/java/com/example/springboot_crud_study/service/ArticleServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.springboot_crud_study.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleServiceTest {
+
+    @Test
+    void index() {
+    }
+}
