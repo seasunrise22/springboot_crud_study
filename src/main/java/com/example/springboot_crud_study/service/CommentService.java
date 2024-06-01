f141cf81d47a0da97ff784345cc1318e83f6fd59
lee <seasunrise22@gmail.com>
2024-06-01 18:30:19 +0900

    댓글 생성 api 요청 200응답 확인

--- src/main/java/com/example/springboot_crud_study/service/CommentService.java
+++ src/main/java/com/example/springboot_crud_study/service/CommentService.java
@@ -1,15 +1,17 @@
 package com.example.springboot_crud_study.service;
 
 import com.example.springboot_crud_study.dto.CommentDto;
+import com.example.springboot_crud_study.entity.Article;
 import com.example.springboot_crud_study.entity.Comment;
 import com.example.springboot_crud_study.repository.ArticleRepository;
 import com.example.springboot_crud_study.repository.CommentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,7 +23,7 @@ public class CommentService {
 
     // 댓글 조회
     public List<CommentDto> comments(Long articleId) {
-        // 1. 댓글 조회
+        /*// 1. 댓글 조회
         List<Comment> comments = commentRepository.findByArticleId(articleId);
         // 2. 엔티티 -> DTO 변환
         List<CommentDto> dtos = new ArrayList<CommentDto>();
@@ -31,6 +33,25 @@ public class CommentService {
             dtos.add(dto);
         }
         // 3. 결과 반환
-        return dtos;
+        return dtos;*/
+
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
+    }
+
+    // 댓글 생성
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " + "대상 게시글이 없습니다."));
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+        // 3. 댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+        // 4. DTO로 변환해 반환
+        return CommentDto.createCommentDto(created);
     }
 }
