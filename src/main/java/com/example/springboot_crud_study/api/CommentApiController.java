d994f577b4821db1352d380ab745920cec1daa07
lee <seasunrise22@gmail.com>
2024-06-02 18:52:52 +0900

    댓글 수정 api 요청 200응답 확인

--- src/main/java/com/example/springboot_crud_study/api/CommentApiController.java
+++ src/main/java/com/example/springboot_crud_study/api/CommentApiController.java
@@ -35,7 +35,17 @@ public class CommentApiController {
         // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(createdDto);
     }
+
     // 3. 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto) {
+        // 서비스에 위임
+        CommentDto updateDto = commentService.update(id, dto);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updateDto);
+    }
+
     // 4. 댓글 삭제
 }
 
